d805a9be655bbef3b386a79daa6675fe39a2d945
Alexey Yudin <alexey.y.yudin@gmail.com>
2019-08-28 16:23:55 +0200

    Add expand/collapse for the description.

--- src/book-item/book-item.js
+++ src/book-item/book-item.js
@@ -4,6 +4,26 @@ import {BookImage} from '../book-image/book-image';
 import './book-item.css';
 
 export class BookItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isExpanded: false
+    };
+  }
+
+  toggleText = () => {
+    this.setState(previousState => {
+      return {
+        isExpanded: !previousState.isExpanded
+      };
+    });
+  };
+
+  getSlicedText = () => {
+    return `${this.props.text.slice(0, 10)}...`;
+  };
+
   render() {
     return (
       <div className='book-item'>
@@ -13,8 +33,13 @@ export class BookItem extends Component {
 
         <div className='book-item__content'>
           <h1 className='book-item__title'>{this.props.title}</h1>
+
           <p className='book-item__text'>
-            {this.props.text}
+            {this.state.isExpanded ? this.props.text : this.getSlicedText()}
+
+            <button type='button' onClick={this.toggleText}>
+              {this.state.isExpanded ? 'Collapse' : 'Expand'}
+            </button>
           </p>
         </div>
       </div>
