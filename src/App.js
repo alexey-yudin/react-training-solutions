da1fbad4d0b2e9439a9fe0b71b787cfd996026c5
Alexey Yudin <alexey.y.yudin@gmail.com>
2019-08-29 11:18:10 +0800

    Fix.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import {Component} from 'react';
 import './App.css';
-import {BookList} from './book-list/book-list';
+import {BookItem} from './book-item/book-item';
 
 export class App extends Component {
+  book = {
+    id: 1,
+    image: '/images/godfather.jpg',
+    title: 'Godfather',
+    description: `With its brilliant and brutal portrayal of the Corleone family,
+      The Godfather burned its way into our national consciousness.
+      This unforgettable saga of crime and corruption, passion and loyalty continues to
+      stand the test of time, as the definitive novel of the Mafia underworld.\n`,
+    price: 100,
+    type: 'hardcover'
+  };
+
   render() {
     return (
       <div className="app">
-        <BookList/>
+        <BookItem
+          image={this.book.image}
+          title={this.book.title}
+          text={this.book.description}
+        />
       </div>
     );
   }
